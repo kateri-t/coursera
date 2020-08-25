ad6abeeeceb9fe12aea5e6b7a487c4adeedf3d24
Kate Simpson <khsimpson@gmail.com>
2020-08-25 23:25:03 +0100

    assignment 4 again

--- module_4_solution/js/SpeakGoodbye.js
+++ module_4_solution/js/SpeakGoodbye.js
@@ -8,7 +8,6 @@
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-// var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 (function (window) {
@@ -25,5 +24,4 @@
 
     // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
     // 'byeSpeaker' on the global scope as well.
-    // xxxx.xxxx = byeSpeaker;
 })(window);
